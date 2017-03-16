1b5385b67057fbfdb59239b6f176e7221a16303d
Richard Cooper <richard.cooper@bennetts.co.uk>
2017-03-16 20:09:36 +0000

    Initial Commit

---
+++ App2/RootViewController.designer.cs
@@ -0,0 +1,14 @@
+﻿using Foundation;
+using System.CodeDom.Compiler;
+
+namespace App2
+{
+    [Register("RootViewController")]
+    partial class RootViewController
+    {
+        void ReleaseDesignerOutlets()
+        {
+        }
+    }
+}
+
